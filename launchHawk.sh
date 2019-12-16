61a58e5acee94b655a647c09fe9d9e3df28400b5
Dominick Rubino <dar220@lehigh.edu>
2019-12-16 00:07:40 +0000

    edited launch script

--- launchHawk.sh
+++ launchHawk.sh
@@ -5,12 +5,6 @@
 
 echo "Starting..."
 
-echo "Attempt to remove existing card"
-
-composer card delete -c PeerAdmin@hlfv1
-
-composer card delete -c admin@hawkoin-network
-
 
 cd ~/fabric-dev-servers
 
@@ -24,7 +18,6 @@ cd ~/HawKoin-Fabric
 
 npm install
 
-
 echo "Moving to ~/HawKoin-Fabric/dist"
 
 cd ~/HawKoin-Fabric/dist
@@ -34,7 +27,16 @@ composer network install -a hawkoin-network.bna -c PeerAdmin@hlfv1
 # NOTE: -V needs to have the latest version in the project's package.json or the command will fail.
 composer network start -n hawkoin-network -V 0.1.5 -c PeerAdmin@hlfv1 -A admin -S adminpw -f networkadmin.card
 
+echo "Attempt to remove existing card"
+
+composer card delete -c admin@hawkoin-network
+
 composer card import --file networkadmin.card
 
-composer-rest-server -c admin@hawkoin-network -n always
+#echo "Launching REST server"
 
+#NOTE: -y specifies an API key is being used
+composer-rest-server -y HawKoinIsAwesome -c admin@hawkoin-network -n always
+
+#NOTE: Use the following command to launch REST server in the background:
+#nohup composer-rest-server -y HawKoinIsAwesome -c admin@hawkoin-network -n always > rest-server.out 2> rest-server.err < /dev/null &
